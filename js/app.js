d8af8841faf517fc1118a52d0a6ab8b20cc23add
Durpadu <wells94e@gmail.com>
2018-11-27 15:47:43 -0800

    added two more y/n questions

--- js/app.js
+++ js/app.js
@@ -38,6 +38,29 @@ var travelGuess = confirm('Do you think I\'ve been to India?');
   if(travelGuess === true) {
     alert('Sorry, but I\'ve never been to India.');
   } else {
-    alert('Thats correct. Never been to India')
+    alert('Thats correct. Never been to India');
   }
 console.log('The user thought ' + travelGuess);
+
+var flavorPref = prompt('Is my favorite flavor of ice cream chocolate?');
+if(flavorPref === 'no' || flavorPref === 'NO' || flavorPref === 'No' || flavorPref === 'nO') {
+  alert('You\'re right, I like a lot of other flavors more than chocolate!');
+} else{
+  alert('No way! Vanilla is better!');
+}
+console.log('user thinks I like chocolate best ' + flavorPref);
+
+var tattooNum = prompt('Do I have more than six tattoos? Y/N').toUpperCase();
+if(tattooNum === 'y'){
+  alert('No, but I wish I did have more!');
+} else{
+  alert('That\'s exactly right!');
+}
+console.log('tatto answer ' + tattooNum);
+var cardGames = prompt('Do I like trading card games? Y/N').toLowerCase();
+if(cardGames === 'y'){
+  alert('Ya! I like games like Magic: The Gathering');
+} else{
+  alert('Actually, I really enjoy trading card games.');
+}
+console.log('nerd level is correct ' + cardGames);
